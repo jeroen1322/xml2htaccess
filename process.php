866333cb8444b5c3821f237dcd0a2f86743464d4
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-05-19 10:48:08 +0200

    Update process.php
    added license comment.

--- process.php
+++ process.php
@@ -3,6 +3,20 @@
 //Author: Jeroen Grooten
 //Description and How to: https://github.com/jeroen1322/xml2htaccess
 
+// xml2htaccess is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// any later version.
+ 
+// xml2htaccess is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+ 
+// You should have received a copy of the GNU General Public License
+// along with xml2htaccess. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+
+
 //XML input form on index.html.
 $input = $_POST['input']; 
 
